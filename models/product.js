86ca447022b79f8ccb5806a05215e2d5f4c43486
Weal Ali <wealali595@gmail.com>
2020-08-09 23:06:30 +0200

    118--119: Extrating + loading product details.

--- models/product.js
+++ models/product.js
@@ -25,6 +25,7 @@ module.exports = class Product {
 
   }
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p,JSON.stringify(products), (error) => {
@@ -35,5 +36,12 @@ module.exports = class Product {
 
   static fetchAll(callbackFunction) {
     getProductsFromFile(callbackFunction);
+  }
+
+  static findById(id, cb){
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product );
+    });
   }
 }
