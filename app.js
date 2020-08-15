8614dc3c9b081651c0f8c31d972676f8c0d0aa52
Weal Ali <wealali595@gmail.com>
2020-08-15 12:20:58 +0200

    141- Retriving Data

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ const db = require('./util/database');
 
 
 const app = express();
-db.execute('SELECT * FROM products');
+db.execute('SELECT * FROM products')
+    .then((result) => {
+        console.log(result[0],result[1]);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+;
 // Configrations settings.
 app.set('view engine', 'ejs');
 app.set('views', 'views');
