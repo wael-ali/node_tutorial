e53ff8db08d1d78fd3d6637b08fbac9853b79e64
Weal Ali <wealali595@gmail.com>
2020-08-15 12:15:59 +0200

    139- Connecting our app to the sql Database

--- app.js
+++ app.js
@@ -2,34 +2,26 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
-
-const rootDir = require('./util/path');
-
-
-const app = express();
-// Configrations settings.
-// app.engine(
-  // 'hbs',
-  //  expressHbs({ 
-      // defaultLayout: 'main-layout',
-      // layoutsDir: 'views/layouts/',
-      // extname: 'hbs'
-    // })
-// );
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-// app.set('view engine', 'hbs');
-// app.set('view engine', 'pug');
-
+// require routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorRoutes = require('./routes/error');
-// template engine 
+
+const rootDir = require('./util/path');
+const db = require('./util/database');
+
+
+const app = express();
+db.execute('SELECT * FROM products');
+// Configrations settings.
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// template engine
 // All Requests Middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-// Other Middleware
+// Routes Middleware
 app.use(adminRoutes);
 app.use(shopRoutes);
 // NOT FOUND PAGE
