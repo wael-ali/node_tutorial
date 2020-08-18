4ec4934fa43748bfb67213fe306431884c8d68e1
Weal Ali <wealali595@gmail.com>
2020-08-18 22:32:33 +0200

    168- Adding Existing Products to the Cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -67,6 +67,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
     let fetchedCart;
+    let newQuantity = 1;
     req.user
         .getCart()
         .then(cart => {
@@ -78,24 +79,21 @@ exports.postCart = (req, res, next) => {
             if (products.length > 0){
                 product = products[0];
             }
-            let newQuantity = 1;
             // adding the same product to the cart.
             if (product){
-               // ...
+               const oldQuantity = product.cartItem.quantity;
+               newQuantity = oldQuantity + 1;
+               return product;
             }
             // adding new product to cart.
-            return Product.findByPk(prodId)
-                .then(product => {
-                    fetchedCart.addProduct(product, {
-                        through: { quantity: newQuantity}
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-            ;
+            return Product.findByPk(prodId);
         })
-        .then(() => {
+        .then((product) => {
+            return fetchedCart.addProduct(product, {
+                through: {quantity: newQuantity}
+            });
+        })
+        .then((result) => {
             res.redirect('/cart');
         })
         .catch(err => {
