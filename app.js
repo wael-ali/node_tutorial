9c7c750487cc648216c073722359c24fa652e4bd
Weal Ali <wealali595@gmail.com>
2020-09-01 08:34:07 +0200

    179-- Installing the MongoDB Driver

--- app.js
+++ app.js
@@ -3,85 +3,39 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 // require routes
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const errorRoutes = require('./routes/error');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
+// const errorRoutes = require('./routes/error');
 
-const rootDir = require('./util/path');
-const sequelize = require('./util/database');
-// Models
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
-
+const mongoConnect = require('./util/database');
 
 const app = express();
 // Configrations settings.
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// template engine
 // All Requests Middleware
 app.use((req, res, next) => {
-    User.findByPk(1)
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    ;
+    // User.findByPk(1)
+    //     .then(user => {
+    //         req.user = user;
+    //         next();
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //     })
+    // ;
+    next();
 })
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
-// Routes Middleware
-app.use(adminRoutes);
-app.use(shopRoutes);
-// NOT FOUND PAGE
-app.use(errorRoutes);
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(express.static(path.join(__dirname, 'public')));
+// // Routes Middleware
+// app.use(adminRoutes);
+// app.use(shopRoutes);
+// // NOT FOUND PAGE
+// app.use(errorRoutes);
 
-// Manage Relations between Models.
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
-User.hasMany(Product)
-User.hasOne(Cart);
-Cart.belongsTo(User);
-// Many-To-Many relation cart-product
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
-// One-To-Many Relationship. (user-order)
-Order.belongsTo(User);
-User.hasMany(Order)
-// Many-To-Many Relation (product-order)
-Order.belongsToMany(Product, { through: OrderItem });
-
-
-
-// to automatically synchronize all models
-sequelize
-    // .sync({ force: true}) // this will drop all tables and force new changes. Dont use it in production.
-    .sync()
-    .then((result) => {
-        // console.log(result);
-        return User.findByPk(1);
-    })
-    .then(user => {
-        if (!user){
-            return User.create({name: 'Max', email: 'test@test.com'});
-        }
-        return user;
-    })
-    .then(user => {
-        console.log(user);
-        return user.createCart();
-    })
-    .then(cart => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    })
-;
+mongoConnect(client => {
+    console.log(client);
+    app.listen(3000);
+})
--- util/database.js
+++ util/database.js
@@ -1,8 +1,22 @@
-const Sequelize = require('sequelize');
 
-const secualize = new Sequelize('node_1', 'root', 'root', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-module.exports = secualize;
+const url = 'mongodb+srv://node_toturial_1:tfO3QFaZHWuAKoJe@cluster0.q4mpc.mongodb.net/test?retryWrites=true&w=majority';
+
+const mongoConnect = (callback) => {
+    MongoClient.connect(url, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+        .then(client => {
+            callback(client);
+        })
+        .catch(err => {
+            console.log('ERROR:',err);
+        })
+    ;
+};
+
+module.exports = mongoConnect;
+
