8dbe5e420ac6317eaac373a187b185b881180569
Weal Ali <wealali595@gmail.com>
2020-09-02 15:57:35 +0200

    198-- Displaying the cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -48,23 +48,20 @@ exports.getIndex = (req, res, next) => {
         })
     ;
 };
-// exports.getCart = (req, res, next) => {
-//     req.user.getCart()
-//         .then(cart => {
-//             return cart.getProducts();
-//         })
-//         .then(products => {
-//             res.render('shop/cart', {
-//               products: products,
-//               pageTitle: 'Your Cart',
-//               path: '/cart',
-//             });
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         })
-//     ;
-// };
+exports.getCart = (req, res, next) => {
+    req.user.getCart()
+        .then(products => {
+            res.render('shop/cart', {
+              products: products,
+              pageTitle: 'Your Cart',
+              path: '/cart',
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    ;
+};
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
     Product.findByID(prodId)
@@ -75,47 +72,6 @@ exports.postCart = (req, res, next) => {
             console.log('XXX_XXX', result);
         })
     ;
-
-
-
-
-
-
-
-    // let fetchedCart;
-    // let newQuantity = 1;
-    // req.user
-    //     .getCart()
-    //     .then(cart => {
-    //         fetchedCart = cart;
-    //         return cart.getProducts({ where: { id: prodId } })
-    //     })
-    //     .then(products => {
-    //         let product;
-    //         if (products.length > 0){
-    //             product = products[0];
-    //         }
-    //         // adding the same product to the cart.
-    //         if (product){
-    //            const oldQuantity = product.cartItem.quantity;
-    //            newQuantity = oldQuantity + 1;
-    //            return product;
-    //         }
-    //         // adding new product to cart.
-    //         return Product.findByPk(prodId);
-    //     })
-    //     .then((product) => {
-    //         return fetchedCart.addProduct(product, {
-    //             through: {quantity: newQuantity}
-    //         });
-    //     })
-    //     .then((result) => {
-    //         res.redirect('/cart');
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
-    //     })
-    // ;
 };
 exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
