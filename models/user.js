9b9a372d0a3814241173b45baff00d9f329c8368
Weal Ali <wealali595@gmail.com>
2020-09-02 09:30:45 +0200

    195-- Working on Cart Items & Orders

--- models/user.js
+++ models/user.js
@@ -1,10 +1,14 @@
 const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
+const ObjectId = mongodb.ObjectId;
+
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart, id) {
         this.username = username;
         this.email = email;
+        this.cart = cart; // {items: []}
+        this._id = id;
     }
 
     save(){
@@ -12,9 +16,21 @@ class User {
         db.collection('users').insertOne(this);
     }
 
+    addToCart(product) {
+         // const cartProduct = this.cart.items.findIndex(cp => {
+         //     return cp._id === product._id;
+         // });
+
+         const updatedCart = {items: [{...product, quality: 1}]}
+         const db = getDb();
+         return db.collection('users').updateOne(
+             { _id: new ObjectId(this._id)},
+             { $set: { cart: updatedCart }}
+         );
+    }
+
     static findByID(id){
         const db = getDb();
-
         return db.collection('users').findOne({ _id: new mongodb.ObjectId(id) });
     }
 }
