6f49c2cc38106f1a5edc63b447752fe2ebf9472d
Weal Ali <wealali595@gmail.com>
2020-09-09 09:39:18 +0200

    225-- Clearign the cart after storing the order

--- controllers/shop.js
+++ controllers/shop.js
@@ -127,10 +127,12 @@ exports.postOrder = (req, res, next) => {
                 },
                 products: products
             });
-
             return order.save();
         })
         .then(result => {
+            return req.user.clearCart();
+        })
+        .then(() => {
             res.redirect('/orders');
         })
         .catch(err => console.log(err))
