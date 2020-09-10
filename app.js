227e3711daa1aea7890d80c14df6156965ac60b8
Weal Ali <wealali595@gmail.com>
2020-09-10 12:54:29 +0200

    253-- Implementing an authentication flow

--- app.js
+++ app.js
@@ -59,19 +59,6 @@ app.use(errorRoutes);
 mongoose
     .connect(MONGODB_URI,  { useNewUrlParser: true,  useUnifiedTopology: true  } )
     .then((result) => {
-        User.findOne()
-            .then(user => {
-                console.log('mongose.connect::::::::::::',user);
-                if (!user){
-                    const user = new User({
-                        name: 'Max',
-                        email: 'max@example.com'
-                    });
-                    user.save();
-                }
-            })
-            .catch(err => console.log(err))
-        ;
         app.listen(3000);
     })
     .catch(err => console.log(err))
--- controllers/auth.js
+++ controllers/auth.js
@@ -39,4 +39,26 @@ exports.getSignup = (req, res, next) => {
         isAuthenticated: false
     })
 };
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+
+    User.findOne({email: email})
+        .then(userDoc => {
+            if (userDoc){
+                return res.redirect('/signup');
+            }
+            const user = new User({
+                email: email,
+                password: password,
+                cart: { items: [] }
+            });
+            return user.save();
+        })
+        .then(result => {
+            res.redirect('/login');
+        })
+        .catch(err => console.log(err))
+    ;
+};
