825fcf30539ffc1464cc22a60a01f062b404eafc
Weal Ali <wealali595@gmail.com>
2020-09-15 13:01:33 +0200

    292-- Builtin and custom validators

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,20 @@ router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
 router.get('/signup', authController.getSignup);
-router.post('/signup', check('email').isEmail().withMessage('Invalid Email'),authController.postSignup);
+router.post(
+    '/signup',
+    check('email')
+        .isEmail()
+        .withMessage('Invalid Email')
+        .custom((value, {req, location}) => {
+            if (value === 'test@test.com'){
+                throw new Error('This is forbidden email..')
+            }
+            return true;
+        })
+    ,
+    authController.postSignup
+);
 router.get('/reset-password', authController.getResetPassword);
 router.post('/reset', authController.postReset);
 router.get('/reset/:token', authController.getNewPassword);
